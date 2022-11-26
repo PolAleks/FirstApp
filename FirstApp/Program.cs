f138d3aadfbbd04607908f6c654c124035baf3df
Александр Полушкин <aa.polushkin@yandex.ru>
2022-11-26 17:40:25 +0300

    Выполнил задание 3.2.8

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string MyName = "Valera";
+            Console.WriteLine(MyName);
+            Console.WriteLine("\tПривет, мир\n\tМне 27 лет\n\tMy name is {0}",MyName);
+            Console.WriteLine("\u0023");
+            Console.ReadKey();
         }
     }
 }
