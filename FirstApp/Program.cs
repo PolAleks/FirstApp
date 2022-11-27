df577d1aaca1c9a098031c7b09074322fd507e3d
Александр Полушкин <aa.polushkin@yandex.ru>
2022-11-27 11:53:46 +0300

    Выполнил задание 3.6.8

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,15 +4,8 @@
     {
         static void Main(string[] args)
         {
-            string MyName = "Александр";
-            byte Age = 40;
-            bool HaveIPet = true;
-            double ShoeSize = 26.5;
-            Console.WriteLine("Меня зовут {0}",MyName);
-            Console.WriteLine("Мне {0}", Age);
-            Console.WriteLine("У тебя есть питомец? {0}", HaveIPet);
-            Console.WriteLine("Размер вашей стопы: {0}", ShoeSize);
-            Console.ReadKey();
+            double result = 10 % 3;
+            Console.WriteLine(result);
         }
         enum Semaphore
         {
