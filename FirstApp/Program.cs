713341345c052c1b78da167ac7de873e287517e1
Александр Полушкин <aa.polushkin@yandex.ru>
2022-11-27 16:57:28 +0300

    Выполнил задание 3.7.4

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,8 +4,14 @@
     {
         static void Main(string[] args)
         {
-            double result = 10 % 3;
-            Console.WriteLine(result);
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+            Console.Write("What your favorit day of week? ");
+            DayOfWeek day = (DayOfWeek)byte.Parse(Console.ReadLine());
+            Console.WriteLine("Your favorit day is {0}", day);
         }
         enum Semaphore
         {
